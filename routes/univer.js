a0a375eb87252d2d2957d599926634f2320139d5
ilusha2012 <ilya.shaydullin@gmail.com>
2015-02-05 21:50:58 +0700

    Прием данных с формы методом пост. Полет норм

--- routes/univer.js
+++ routes/univer.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Univer = require('models/univer').Univer;
+var bodyParser = require('body-parser');
 
 router.get('/', function(req, res, next) {
     res.render('univer', { title: 'Univer' });
 });
 
 router.post('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+
+
+ /*   Univer.find({name: req.body.name}, function(err, data){
+        if (err) return next(err);
+        res.send("Такой универ уже есть!");
+    });*/
+    if (!req.body) {res.sendStatus(400)
+    }else{
+        res.end(req.body.nameofUniver);
+    }
 });
 
 module.exports = router;
