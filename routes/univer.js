682bced7c24211c3616f6eb5e70befc9a38a5c7e
ilusha2012 <ilya.shaydullin@gmail.com>
2015-02-05 20:46:44 +0700

    New view univer.jade

--- routes/univer.js
+++ routes/univer.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('univer', { title: 'Univer' });
 });
 
 router.post('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
+module.exports = router;
