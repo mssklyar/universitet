9c475e982677fe1440ea4661933a6170415488f4
Max <ravonta@yandex.ru>
2015-02-09 20:47:51 +0600

    Вьюхи главной и профиля

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 module.exports = function(app){
-   app.use('/', require('routes/frontpage'));
+   app.use('/', require('routes/main'));
    app.use('/users', require('routes/users'));
    app.use('/univer', require('routes/univer'));
    app.use('/login', require('routes/login'));
    app.use('/logout', require('routes/logout'));
+   app.use('/main', require('routes/frontpage'));
 }
